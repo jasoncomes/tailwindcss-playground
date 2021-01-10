494479e8092efe69f30bfd218d36893626d76728
Jason Comes <jasonwcomes@gmail.com>
2021-01-09 20:25:58 -0600

    Remove split chunks for playground

--- webpack.config.js
+++ webpack.config.js
@@ -93,33 +93,8 @@ module.exports = {
       },
     ],
   },
-
-  /*
-   * SplitChunksPlugin is enabled by default and replaced
-   * deprecated CommonsChunkPlugin. It automatically identifies modules which
-   * should be splitted of chunk by heuristics using module duplication count and
-   * module category (i. e. node_modules). And splits the chunks.
-   *
-   * It is safe to remove "splitChunks" from the generated configuration
-   * and was added as an educational example.
-   *
-   * https://webpack.js.org/plugins/split-chunks-plugin/
-   *
-   */
   optimization: {
     minimize: process.env.NODE_ENV === 'production',
     minimizer: [new TerserPlugin()],
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          priority: -10,
-          test: /[\\/]node_modules[\\/]/,
-        },
-      },
-      chunks: 'async',
-      minChunks: 1,
-      minSize: 30000,
-      name: false,
-    },
   },
 };
